db6f88308fed2f009af507a5fff521850f9dc6a6
Leon Acero <acero@hotmail.com>
2022-12-13 19:53:30 -0600

    Deploy 13 de Diciembre 2022

--- src/components/splashScreenAlt/SplashScreenAlt.jsx
+++ src/components/splashScreenAlt/SplashScreenAlt.jsx
@@ -1,26 +1,46 @@
-import { Link } from 'react-router-dom'
 import "./splashScreenAlt.css"
+import { Link } from 'react-router-dom'
+
+import { domAnimation, LazyMotion, m } from 'framer-motion';
+
+const svgVariants = {
+  hidden: { 
+    opacity: 0, 
+  },
+  visible: { 
+    opacity: 1, 
+    transition: { delay: .5, duration: 2 }
+  }
+};
 
 function SplashScreenAlt() {
   return (
-    <div className='splashScreenAlt'>
-      <div className='splashScreenAlt__leftPanel'>
-        <div className='laptops'>
+    <LazyMotion features={domAnimation} >
+      <m.div 
+            className='splashScreenAlt'
+            variants={svgVariants}
+            initial="hidden"
+            animate="visible" 
+      >
+        <div className='splashScreenAlt__leftPanel'>
+          <div className='laptops'>
 
-          <h3 className='splashScreenAlt__subTitulo'>EL JUANJO</h3>
+            <h3 className='splashScreenAlt__subTitulo'>EL JUANJO</h3>
 
-          <div className='container_titulo'>
-            <h1 className='splashScreenAlt__titulo'>Satisfacemos la pasión</h1>
-            <h1 className='splashScreenAlt__titulo'>por los dulces</h1>
+            <div className='container_titulo'>
+              <h1 className='splashScreenAlt__titulo'>Satisfacemos la pasión</h1>
+              <h1 className='splashScreenAlt__titulo'>por los dulces</h1>
+            </div>
+            <div className='container_slogan'>
+              <p className='slogan'>Llevamos los dulces más deliciosos a tiendas y negocios.</p>
+            </div>
           </div>
-          <div className='container_slogan'>
-            <p className='slogan'>Llevamos los dulces más deliciosos a tiendas y negocios.</p>
-          </div>
+          <Link className='hero__loginButton' to="/login">Iniciar sesión</Link>
         </div>
-        <Link className='hero__loginButton' to="/login">Iniciar sesión</Link>
-      </div>
-      <img className='splashScreenAlt__mujer-modelo' src="/img/website/modelo-con-paleta-recortado.png" alt="" />
-    </div>
+        <img className='splashScreenAlt__mujer-modelo' src="/img/website/modelo-con-paleta-recortado.png" alt="" />
+      </m.div>
+
+    </LazyMotion>
   )
 }
 
